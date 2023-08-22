7b6b5109de672b59bfb937ba5495a4ea8af54203
yultrish <yultrish@gmail.com>
2023-08-22 23:38:44 +0000

    update

--- profile/users.js
+++ profile/users.js
@@ -16,13 +16,18 @@ window.addEventListener('load', async () => {
                     userData += `
                     <div class="users">
                     <div class="user">
+                        <h3>${user.id}</h3>
                         <h3>${user.firstname} ${user.lastname}</h3>
                         <h3>${user.email}</h3>
                         <h3>${user.school}</h3>
+                        <h3>${user.contact}</h3>
                     </div>
                     <div class="icons">
                         <span class="material-symbols-outlined">
                         delete
+                        </span>
+                        <span class="material-symbols-outlined">
+                        library_books
                         </span>
                    </div>
                    </div>
